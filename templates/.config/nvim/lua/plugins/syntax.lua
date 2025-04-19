feb773807ce79b941b02eec4ded698839724c506
Dick Davis <dick@hey.com>
2025-04-19 16:03:38 -0500

    Fix treesitter indentation in Ruby

--- templates/.config/nvim/lua/plugins/syntax.lua
+++ templates/.config/nvim/lua/plugins/syntax.lua
@@ -17,22 +17,22 @@ return {
         "css",
         "yaml",
         "json",
+        "terraform",
       },
-
+      ignore_install = {},
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
-
       -- Automatically install missing parsers when entering buffer
       auto_install = true,
-
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
       },
-
       indent = {
         enable = true,
+        disable = { "ruby" }
       },
+      modules = {}
     })
   end,
 }
